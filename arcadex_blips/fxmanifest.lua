acaa44402fef457795697a7ded30e8527c5ebd95
Arcadex <96445889+ArcadexScripts@users.noreply.github.com>
2025-08-06 19:27:31 -0400

    Add files via upload

---
+++ arcadex_blips/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zed'
+description 'A simple blip creation system!'
+version '1.0.0'
+
+shared_scripts {
+  'config.lua'
+}
+
+client_scripts {
+  'client.lua',
+}
